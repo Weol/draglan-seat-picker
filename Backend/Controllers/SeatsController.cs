43ec6cd117d34df96236a1e100ec74aab8d61d00
Erik Rahka <erikrahka@gmail.com>
2021-10-05 22:03:59 +0200

    Initial commit

---
+++ Backend/Controllers/SeatsController.cs
@@ -0,0 +1,28 @@
+﻿using DragLanSeatPicker.Models.Seat;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Backend.Controllers 
+{
+    public static class SeatsController
+    {
+        [FunctionName("PutSeat")]
+        public static IActionResult PutSeat(
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = "seats/{id:int}")] HttpSeatModel seat, 
+            [CosmosDB(databaseName: "ToDoItems", collectionName: "Items", ConnectionStringSetting = "CosmosDBConnection")] out dynamic document,
+            ILogger log)
+        {
+            document = new CosmosSeatModel
+            {
+                Id = seat.Id, 
+                Mail = seat.Mail,
+                Name = seat.Name,
+                Password = seat.Password
+            };
+
+            return new AcceptedResult();
+        }
+    }
+}
---
+++ Backend/Models/Seat/CosmosSeatModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DragLanSeatPicker.Models.Seat
+{
+    public class CosmosSeatModel : HttpSeatModel
+    {
+        [JsonProperty("partitionKey")]
+        public string PartitionKey => "partitionKey";
+    }
+}
