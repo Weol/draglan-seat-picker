e2a3039aa8cf666a6644b0111fd1e8bbbe8357a0
erik.nysto.rahka <erikrahka@gmail.com>
2021-11-24 20:35:32 +0100

    dasds

--- frontend/src/express.js
+++ frontend/src/express.js
@@ -1,9 +1,6 @@
 ﻿const express = require('express');
 const path = require('path');
-const cors = require('cors');
 const app = express();
-
-app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
