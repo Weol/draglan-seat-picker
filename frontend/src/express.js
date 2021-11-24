79e393862a2ddd218d4fd2801fcc00475ddcf7f4
erik.nysto.rahka <erikrahka@gmail.com>
2021-11-24 22:26:12 +0100

    asfs

--- frontend/src/express.js
+++ frontend/src/express.js
@@ -1,15 +1,20 @@
 ﻿const express = require('express');
+const proxy = require('express-http-proxy');
 const app = express();
+
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*')
     next();
 });
 
+
 app.use(express.static('/home/site/wwwroot'));
 
 app.get('/*', function (req, res) {
     res.sendFile('/home/site/wwwroot/index.html');
 });
+
+app.use('/api', proxy('www.seatpicker-backend.azurewebsites.net'));
 
 app.listen(8080);
