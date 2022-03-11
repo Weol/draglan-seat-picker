d5e288f5638553f5c4aebd7806911a3146df80d3
Erik Rahka <erikrahka@gmail.com>
2022-03-12 00:01:52 +0100

    Force https

--- frontend/src/express.js
+++ frontend/src/express.js
@@ -2,6 +2,14 @@
 const proxy = require('express-http-proxy');
 const app = express();
 
+app.use(function(request, response, next) {
+    if (!request.secure) {
+        return response.redirect("https://" + request.headers.host + request.url);
+    }
+
+    next();
+})
+
 app.use(express.static('/home/site/wwwroot'));
 app.use('/api', proxy('seatpicker-backend.azurewebsites.net'));
 
